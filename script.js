4aab4dce081c825980d56ad85807c1951d755f11
jvendramin <jacob@vendramin.work>
2023-06-26 21:38:43 -0700

    Fix formatting of response from OpenAI POST request

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('post-btn');
 
-const openAIKey = 'Bearer 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';
+const openAIKey = 'Bearer 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';
 const openAIHeaders = {
     'Authorization':openAIKey,
     'Content-Type':'application/json'
@@ -8,7 +8,7 @@ const openAIHeaders = {
 
 const payload = {
     "model": "text-davinci-003",
-    "prompt": "Step 1: Return a list titled 'Important Job Posting Buzzwords' of 7 crucial buzzwords from this job posting: https://www.linkedin.com/jobs/view/3638865516/?refId=44043183-1975-4e4f-aa09-9cb143b34ade&trackingId=8Mk7jN0hTsWCusWaKHcnOg%3D%3D. Step 2: Use this resume: https://drive.google.com/file/d/1nPj0mWpE7e5xZEE8IMNmDbNYfLpjaknd/view?usp=sharing to create a second list titled: 'Job Experience Section Suggestions' of 4-7 suggestions of how to word the job experience section to be relevant to the included job posting. Step 3: Return both lists in HTML format. Step 4: Use '<h3>' tags for the list titles. Step 5: Trim the '/n/n' at the beginning of your response, the very first item in your response should be '<h3>', exlcude new lines at the beginning of your response.",
+    "prompt": "Step 1: Return a list titled 'Important Job Posting Buzzwords' of 7 crucial buzzwords from this job posting: https://www.linkedin.com/jobs/view/3638865516/?refId=44043183-1975-4e4f-aa09-9cb143b34ade&trackingId=8Mk7jN0hTsWCusWaKHcnOg%3D%3D. Step 2: Use this resume: https://drive.google.com/file/d/1nPj0mWpE7e5xZEE8IMNmDbNYfLpjaknd/view?usp=sharing to create a second list titled: 'Job Experience Section Suggestions' of 4-7 suggestions of how to word the job experience section to be relevant to the included job posting. Step 3: Return both lists in HTML format using <ul>. Step 4: Use <h3> tags for the list titles.",
     "max_tokens": 3800,
     "temperature": 0.7
 };
@@ -21,6 +21,7 @@ const getOpenAI = async () => {
         headers: openAIHeaders
         });
     let openAIJSON = await openAIRes.json();
+    //console.log(openAIJSON)
 
     return openAIJSON;
 };
@@ -28,9 +29,8 @@ const getOpenAI = async () => {
 window.onload = async function() {
     openAIJSONResp = await getOpenAI();
 
-    htmlResp = openAIJSONResp.choices[0].text;
-    document.getElementById("resp").innerHTML = htmlResp;
-    console.log(openAIJSONResp)
-};
+    htmlResp = openAIJSONResp.choices[0].text.replace(/\n/g,'');;
 
-console.log(onload.getOpenAI)
+    console.log(htmlResp)
+    document.getElementById("resp").innerHTML = htmlResp;
+};
